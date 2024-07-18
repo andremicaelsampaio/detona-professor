9feed7639c172a64508d1d1aec0c9ea483ba1248
andremicaelsampaio <andremicaelsam@icloud.com>
2024-07-17 21:25:56 -0400

    Adicionado: Classe curso

---
+++ detona-professor/src/main/java/br/ufmt/alg3/curso/Curso.java
@@ -0,0 +1,12 @@
+package br.ufmt.alg3.curso;
+
+public class Curso {
+    
+    String nome;
+    String periodo;
+    float nota;
+    String comentario;
+    String rga;
+    String Semestre;
+
+}
