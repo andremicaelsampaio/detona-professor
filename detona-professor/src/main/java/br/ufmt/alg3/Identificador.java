c500de12bbc34c2e9cfdc77c888ed89be93f5be7
andremicaelsampaio <andremicaelsam@icloud.com>
2024-08-07 20:44:30 -0400

    Funcionando

--- detona-professor/src/main/java/br/ufmt/alg3/Identificador.java
+++ detona-professor/src/main/java/br/ufmt/alg3/Identificador.java
@@ -6,6 +6,30 @@ public class Identificador {
     private float nota; // Estrela 0 a 5 fracionada
     private String comentario;
     private String semestre; // 2024/1
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public float getNota() {
+        return nota;
+    }
+    public void setNota(float nota) {
+        this.nota = nota;
+    }
+    public String getComentario() {
+        return comentario;
+    }
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+    public String getSemestre() {
+        return semestre;
+    }
+    public void setSemestre(String semestre) {
+        this.semestre = semestre;
+    }
 
 
 }
